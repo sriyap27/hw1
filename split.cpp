9d0f75c8b10a331783fda46efd2dae0ea3419182
Sriya Pratipati <pratipat@usc.edu>
2025-01-30 09:00:35 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -13,11 +13,46 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+void addNode(Node*& l, int val);
+
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if (in == nullptr) {
+    return;
+  }
+  if (in->value % 2 == 1) {
+    if (odds == nullptr) {
+      Node* odd = new Node(in->value, nullptr);
+      odds = odd;
+    }
+    else {
+      addNode(odds, in->value);
+    }
+  }
+  else {
+    if (evens == nullptr) {
+      Node* even = new Node(in->value, nullptr);
+      evens = even;
+    }
+    else {
+      addNode(evens, in->value);
+    }
+  }
+  Node* temp = in;
+  in = in->next;
+  delete temp;
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+void addNode(Node*& l, int val) {
+  if (l == nullptr) {
+    l = new Node(val, nullptr);
+  }
+  else {
+    addNode(l->next, val);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,27 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-
+  Node* list = new Node(1, nullptr);
+  list->next = new Node(2, nullptr);
+  list->next->next = new Node(3, nullptr);
+  Node* even = nullptr;
+  Node* odd = nullptr;
+  // while (list != nullptr) {
+  //   cout << list->value << endl;
+  //   list = list->next;
+  // }
+  split(list, odd, even);
+  while (even != nullptr) {
+    cout << even->value << endl;
+    even = even->next;
+  }
+  while (odd != nullptr) {
+    cout << odd->value << endl;
+    odd = odd->next;
+  }
 }
